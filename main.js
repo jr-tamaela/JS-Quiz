b869f81e170115416c94f1c34144da90db141086
Justin Roy <justin.roy.tamaela@mendix.com>
2023-01-25 19:33:42 +0100

    added function to check if answer are right or wrong.

--- main.js
+++ main.js
@@ -19,3 +19,29 @@ const disable = (button) => {
 };
 
 const enable = (button) => button.removeAttribute("disabled");
+
+//TODO 5
+const isCorrect = (guessString) => {
+  return guessString === fact.answer.toString();
+};
+
+//TODO 6
+for (let button of optionButtons) {
+  button.addEventListener("click", (event) => {
+    console.log("clicked");
+
+    // display the fact's explanation by setting the text of the explanation element.
+    explanation.textContent = fact.explanation;
+
+    // disable all the option buttons
+    for (let disableButtons of optionButtons) {
+      disable(disableButtons);
+    }
+
+    if (isCorrect(button.value)) {
+      button.classList.add("correct");
+    } else {
+      button.classList.add("incorrect");
+    }
+  });
+}
